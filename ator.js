e8d0b7e3241e2191a53bbc624a624367706dd0da
Leonardo <detrozleonardo@gmail.com>
2022-07-13 17:55:46 -0300

    atualizando códigos

---
+++ ator.js
@@ -0,0 +1,54 @@
+// Variaveis Ator
+let yAtor = 366;
+let xAtor = 85;
+let colisao = false;
+let meusPontos = 0;
+
+function mostraAtor() {
+    image(imagemDoAtor, xAtor, yAtor, 30, 30);
+}
+
+function movimentaAtor() {
+    if (keyIsDown(UP_ARROW)) {
+        yAtor -= 2;
+    }
+    if (keyIsDown(DOWN_ARROW) && yAtor < 366) {
+        yAtor += 2;
+    }
+}
+
+function verificaColisao() {
+    for (let i = 0; i < imagemCarros.length; i++) {
+        colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15);
+        if (colisao) {
+            voltaAtor();
+            somDaColisao.play();
+            if (pontosMaiorQueZero()) {
+                meusPontos -= 1;
+            }
+        }
+    }
+}
+
+function voltaAtor() {
+    yAtor = 366;
+}
+
+function pontosMaiorQueZero() {
+    return meusPontos > 0;
+}
+
+function incluiPontos() {
+    textAlign(CENTER);
+    textSize(25);
+    fill(color(255, 240, 60));
+    text(meusPontos, width /5, 27);
+}
+
+function marcaPonto() {
+    if (yAtor < 12) {
+        meusPontos++;
+        somDoPonto.play();
+        voltaAtor();
+    }
+}
